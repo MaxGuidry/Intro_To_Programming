f2279c32252cf6cd1c3aff4e2471acca3ec4c3e5
MaxGuidry <mguidry7@outlook.com>
2016-08-18 11:58:08 -0500

    All basic math operations
    Made one operation for each basic math type:
    adding, subtracting, multiplying, dividing, raising to a power, and
    square rooting.
    All operations are stored in a function called mathOperations

--- Variables/Variables.cpp
+++ Variables/Variables.cpp
@@ -1,14 +1,30 @@
 #include<iostream>
+#include<math.h> //imports sqrt and pow 
+int mathOperations();
 
 int main()
 {
-	int number = 5;
-	{
-		int x = 10;
-		number = x;
-	}
-
-	std::cout << number << std::endl;
+	mathOperations();
 	system("pause");
+	return 0;
+}
+
+
+
+int mathOperations()
+{
+	int add = 5 + 5;
+	int subtract = 12 - 6;
+	int multiply = 2 * 20;
+	int divide = 500 / 5;
+	int power = pow(3, 2);
+	int squareRoot = sqrt(25);
+
+	std::cout << add << std::endl;
+	std::cout << subtract << std::endl;
+	std::cout << multiply << std::endl;
+	std::cout << divide << std::endl;
+	std::cout << power << std::endl;
+	std::cout << squareRoot << std::endl;
 	return 0;
 }
