ceeb7ef5bfddd33ab54d49247710afa399012933
MaxGuidry <mguidry7@outlook.com>
2016-09-02 17:54:07 -0500

    nohting

--- Classes/Classes.cpp
+++ Classes/Classes.cpp
@@ -3,11 +3,13 @@
 #include<Windows.h>
 #include<time.h>
 #include "ClassFile.h"
-
+#define underline "1Bh[4m";
 
 
 int main()
 {
+
+	underline std::cout << "jdjdjdjdjdfaskldfsad" << std::endl;
 	Zombie Donray{ 100,5,"Bad Programmer", "Donray" };
 	Zombie Max{ 200,20,"Good Programmer", "Max" };
 	for (int Turns = 1; Turns < 6; Turns++)
