a2acf39acdef61e42e7085422585af0c1b14402a
MaxGuidry <mguidry7@outlook.com>
2016-10-13 09:31:49 -0500

    morning gist

--- Morning/Source.cpp
+++ Morning/Source.cpp
@@ -1,18 +1,32 @@
 #include<iostream>
-void swap(char &,char &);
+int sum(int*, int);
 
 int main()
 {
-	char first = 'a';
-	char second = 'b';
-	swap(first, second);
+	int nums[5] = { 1,4,6,5,2 };
+	int thing = sum(nums, 5);
 	system("pause");
 	return 0;
 }
-
-void swap(char &a, char &b)
+int sum(int * array, int size)
 {
-	a = a + b;
-	b = a - b;
-	a = a - b;
+	int largest = array[0];
+	int second;
+	for (int i = 1; i < size; i++)
+	{
+		if (array[i] == largest)
+		{
+			second = array[i];
+		}
+		else if (array[i] > largest)
+		{
+			second = largest;
+			largest = array[i];
+		}
+		if (array[i] > second&&array[i] != largest)
+		{
+			second = array[i];
+		}
+	}
+	return second + largest;
 }
