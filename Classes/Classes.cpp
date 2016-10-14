b806bff189874789374be63f15a6b4e674b21fb6
MaxGuidry <mguidry7@outlook.com>
2016-10-14 09:25:34 -0500

    getting rid of useless stuff

--- Classes/Classes.cpp
+++ Classes/Classes.cpp
@@ -2,29 +2,9 @@
 #include<string>
 #include<Windows.h>
 #include<time.h>
-#include "ClassFile.h"
+#include "PracticeClass.h"
 
-//
-//class student
-//{
-//public:
-//	student() {};
-//	student(std::string name, int age)
-//	{
-//		m_Name = name;
-//		m_Age = age;
-//	}
-//	std::string m_Name;
-//	int m_Age;
-//};
-//
-//bool swap(student &a, student&b)
-//{
-//	student temp = a;
-//	a = b;
-//	b = temp;
-//	return true;
-//}
+
 
 class Math
 {
@@ -35,24 +15,7 @@ public:
 
 int main()
 {
-	/*asd
-		Zombie Donray{ 100,5,"Bad Programmer", "Donray" };
-		Zombie Max{ 200,20,"Good Programmer", "Max" };
-		for (int Turns = 1; Turns < 6; Turns++)
-		{
-			Donray.Attack(Max);
-		}
-		items book = items("Book", .5f, true, 5);
-		inventory test;
-		test.addItem(book);
-		*/
-
-		/*student Max = student( "Max", 18 );
-		student Donray = student("Donray", 90);
-		student students[2] = { Max, Donray };
-		swap(students[0], students[1]);*/
-
-
+	
 	int num = Math::add(1, 2);
 	system("pause");
 	return 0;
---
+++ Classes/PracticeClass.cpp
@@ -0,0 +1,2 @@
+#include"PracticeClass.h"
+
