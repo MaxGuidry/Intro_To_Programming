9fea68cccc6eb0fe252b96b5180693df8e6e2d60
MaxGuidry <mguidry7@outlook.com>
2016-10-16 13:36:07 -0500

    Weapon Class

--- Classes/Classes.cpp
+++ Classes/Classes.cpp
@@ -7,36 +7,28 @@
 #include"PracticeClass.h"
 
 
-template<typename T>
-class Bucket
-{
-public:
-	Bucket(int size) :m_size(size) {};
-	bool Add(T a);
-	int m_size;
-	T * items = new T;
-};
+//template<typename T>
+//class Bucket
+//{
+//public:
+//	Bucket(int size) :m_size(size) {};
+//	bool Add(T a);
+//	int m_size;
+//	T * items = new T;
+//};
+//
+//template<class T>
+//bool Bucket<T>::Add(T a)
+//{
+//	items[0] = a;
+//	return true;
+//}
+//
 
-template<class T>
-bool Bucket<T>::Add(T a)
-{
-	items[0] = a;
-	return true;
-}
-
-class Weapon
-{//TODO templates/overides/inheritance
-public:
-	float DamagePerSecond();
-private:
-	float m_minimumDamage;
-	float m_maximumDamage;
-	float m_attackSpeed;
-};
 
 int main()
 {
-	Bucket<int> nums = Bucket<int>(10);
+	/*Bucket<int> nums = Bucket<int>(10);
 	int thingy = 2;
 	nums.Add(thingy);
 	Item first = Item("first");
@@ -44,7 +36,12 @@ int main()
 	if (first == second)
 	{
 		std::cout << "it worked\n";
-	}
+	}*/
+
+	RangedWeapon Bow = RangedWeapon(25.6364, 13.98, 1.345,"Terrible Bow");
+	float DPS = Bow.DamagePerSecond();
+	Inventory<Weapon> backpack=Inventory<Weapon>(50);
+	backpack.Add(Bow);
 	system("pause");
 	return 0;
 }
--- Classes/PracticeClass.h
+++ Classes/PracticeClass.h
@@ -3,17 +3,71 @@
 #include<iostream>
 #include<Windows.h>
 #include<time.h>
-class Item
+
+template<typename T>
+class Inventory
 {
 public:
-	bool operator ==(const Item& theItem)const;
-	Item(std::string name) :m_name(name) {};
+	Inventory(int size) :m_size(size) {};
+	bool Add(T a);
+	int m_size;
+	T * items = new T;
+};
+template<typename T>
+bool Inventory<T>::Add(T a)
+{
+	items[0] = a;
+	return true;
+}
+class Weapon
+{//TODO templates/overides/inheritance
+public:
+	Weapon() {}
+	Weapon(float minDamage, float maxDamage, float atkSpeed,std::string name) :m_minimumDamage(minDamage), m_maximumDamage(maxDamage), m_attackSpeed(atkSpeed),m_name(name) {}
+	float DamagePerSecond();
+	bool operator ==(const Weapon&thisWeapon)const;
+private:
+	float m_minimumDamage;
+	float m_maximumDamage;
+	float m_attackSpeed;
 	std::string m_name;
 };
-
-bool Item::operator ==(const Item& theItem)const
+class RangedWeapon :public Weapon
 {
-	if (m_name == theItem.m_name)
+public:
+	RangedWeapon(float minDamage, float maxDamage, float atkSpeed,std::string name):Weapon(minDamage,maxDamage,atkSpeed,name){}
+private:
+
+};
+class MeleeWeapon :public Weapon
+{
+public:
+	MeleeWeapon(float minDamage, float maxDamage, float atkSpeed, std::string name) :Weapon(minDamage, maxDamage, atkSpeed, name) {}
+private:
+};
+float Weapon::DamagePerSecond()
+{
+	return ((m_maximumDamage + m_minimumDamage)/2) / m_attackSpeed;
+}
+bool Weapon::operator ==(const Weapon&thisWeapon)const
+{
+	if (m_attackSpeed == thisWeapon.m_attackSpeed&&m_maximumDamage == thisWeapon.m_maximumDamage&&m_minimumDamage == thisWeapon.m_minimumDamage&&m_name==thisWeapon.m_name)
 		return true;
 	return false;
 }
+//
+//
+//class Item
+//{
+//public:
+//	bool operator ==(const Item& theItem)const;
+//	Item(std::string name) :m_name(name) {};
+//	std::string m_name;
+//};
+//
+//bool Item::operator ==(const Item& theItem)const
+//{
+//	if (m_name == theItem.m_name)
+//		return true;
+//	return false;
+//}
