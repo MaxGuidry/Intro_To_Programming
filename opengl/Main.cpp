34da7163c50fbe2da0ec06fd51ba77e92f1da95f
MaxGuidry <mguidry7@outlook.com>
2017-02-19 19:34:38 -0600

    testing circles and trying to figure out 3d shapes

--- opengl/Main.cpp
+++ opengl/Main.cpp
@@ -8,21 +8,40 @@
 * The point (0.0, 0.0) represents the middle of the window (not the top left corner).
 * The "2f" suffix means 2 values of float type (x and y).
 */
-void displayMe(void) {
+
+void Poly3dTest()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glVertex3f(0, 0, 0);
+	glVertex3f(.5, 0, 0);
+	glVertex3f(0, .5, .5);
+	glVertex3f(0, 0, 0);
+	glVertex3f(-.2, 0, 0);
+	glVertex3f(-.2, .2, .2);
+	glVertex3f(0, 0, 0);
+
+	glEnd();
+	glFlush();
+
+
+}
+void CircleFunction(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
 	float test = 0;
-	for (float i = -1; i <=1 ; i+=.001)
+	for (float i = -sqrt(.75); i <= sqrt(.75); i+=.01)
 	{
 	
-		float x = sqrt(-((float)i*(float)i) + (float)1);
+		float x = sqrt(-((float)i*(float)i) + (float).75);
+		std::cout << x << " , " << i<<"\n";
 		glVertex2f(i, x);
 		test = x;
 	}
 
-	for (float i = 1; i >= -1; i -= .001)
+	for (float i = sqrt(.75); i >= -sqrt(.75); i -= .01)
 	{
-		float y = -sqrt(-((float)i*(float)i) + (float)1);
+		float y = -sqrt(-((float)i*(float)i) + (float).75);
 	
 		glVertex2f(i, y);
 	}
@@ -37,7 +56,7 @@ int main(int argc, char** argv) {
     glutInitWindowSize(1000, 1000);                    // window size
     glutInitWindowPosition(0, 0);                // distance from the top-left screen
     glutCreateWindow("BadproG - Hello world :D");    // message displayed on top bar window
-    glutDisplayFunc(displayMe);
+    glutDisplayFunc(Poly3dTest);
     glutMainLoop();
     return 0;
 }
